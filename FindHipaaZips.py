b6c3d8c6190105c55bf9f30c7c23935db10a4589
kalimist123 <kali.mist@gmail.com>
2019-04-15 21:59:56 +0100

    find hipaa zips

---
+++ FindHipaaZips.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import re
+searchlist = []
+with open("forbidden_zips.txt") as g:
+    for line in g:
+        searchlist.append(line.strip())
+
+pattern = re.compile("|".join(searchlist))
+
+df = pd.read_csv("filename.csv",encoding = 'unicode_escape')
+rx = df[df['zip'].astype(str).str.contains(pattern)==True]
+print(rx)
