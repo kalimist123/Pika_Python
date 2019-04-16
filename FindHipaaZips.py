9e3d651b7e5d99562da275ba4a9f9aa44c86ee0f
kalimist123 <kali.mist@gmail.com>
2019-04-16 20:52:21 +0100

    Update FindHipaaZips.py

--- FindHipaaZips.py
+++ FindHipaaZips.py
@@ -8,6 +8,6 @@ with open("forbidden_zips.txt") as g:
 pattern = re.compile("|".join(searchlist))
 
 
-df = pd.read_csv(r"Diaceutics 041417_043017_deid.xlsx .csv",encoding = 'unicode_escape')
+df = pd.read_csv(r"file.xlsx .csv",encoding = 'unicode_escape')
 rx = df[df['zip'].astype(str).str.contains(pattern)==True]
 print(rx)
